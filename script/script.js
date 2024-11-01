d73b839bc4524e1926f5fd78daf730c5c03de368
manik1312006 <manikmehtaji@gmail.com>
2024-11-01 21:16:38 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 const movies = [
+    { title: "Bhool Bhulaiyaa 3[2024]", image: "https://m.media-amazon.com/images/M/MV5BOTk1MDQxZTQtMTI0OC00ZTc5LTgxNjgtNzhmY2M4ZWY0OTgyXkEyXkFqcGc@._V1_.jpg", link: "" },
+
     { title: "Singham Again[2024]", image: "https://m.media-amazon.com/images/M/MV5BMjQzZDExZDEtYjAxYy00ZGVhLWE4YWItNTVkZjA5ZjVjZWM3XkEyXkFqcGc@._V1_.jpg", link: "https://firebasestorage.googleapis.com/v0/b/ip-tv-1312.appspot.com/o/Singham%20Again%20(2024).mkv?alt=media&token=6802808e-c050-4524-a93e-3cd298c18a1b" },
 
     { title: "The Guilty [2021]", image: "https://m.media-amazon.com/images/M/MV5BZmVjNzM0YmItZGQwYy00MzA4LTkwYjAtMmJkNWVmOTRkZGRkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg", link: "html/the guilty.html" },
